944d4039c12c0879ec870d7686951c6fd735c9a3
srishti-3 <srishti.8055@iitg.ac.in>
2024-05-10 09:02:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function changeColorB31(){
+    let btn=document.getElementById('dc1');
+    let box=document.getElementById("box3");
+    let btn2=document.getElementById('dc2')
+    box.style.backgroundColor='green';
+    btn.style.display='none';
+    btn2.style.display='none';
+    
+}
+
+function changeColorB32(){
+    let btn=document.getElementById('dc2');
+    let box=document.getElementById("box3");
+    let btn2=document.getElementById('dc1')
+    box.style.backgroundColor='red';
+    btn.style.display='none';
+    btn2.style.display='none';
+    
+}
+
+function changeColorB21(){
+    let btn=document.getElementById('cn1');
+    let box=document.getElementById("box2");
+    let btn2=document.getElementById('cn2')
+    box.style.backgroundColor='green';
+    btn.style.display='none';
+    btn2.style.display='none';
+    
+}
+
+function changeColorB22(){
+    let btn=document.getElementById('cn1');
+    let box=document.getElementById("box2");
+    let btn2=document.getElementById('cn2')
+    box.style.backgroundColor='red';
+    btn.style.display='none';
+    btn2.style.display='none';
+    
+}
+
+function changeColorB11(){
+    let btn=document.getElementById('dsa1');
+    let box=document.getElementById("box1");
+    let btn2=document.getElementById('dsa2')
+    box.style.backgroundColor='green';
+    btn.style.display='none';
+    btn2.style.display='none';
+    
+}
+
+function changeColorB12(){
+    let btn=document.getElementById('dsa1');
+    let box=document.getElementById("box1");
+    let btn2=document.getElementById('dsa2')
+    box.style.backgroundColor='red';
+    btn.style.display='none';
+    btn2.style.display='none';
+    
+}
